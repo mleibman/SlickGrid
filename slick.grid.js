ff1e7cf746495f837b2aa67ff5f10edc770dadcc
Michael Leibman <michael.leibman@gmail.com>
2012-01-23 10:34:21 -0800

    Another follow-up fix to https://github.com/mleibman/SlickGrid/commit/98a503f3048591dbd6ae3ff8565e5319b97cf37c - fixed an issue with empty grid display when forceFitColumns=true.

--- slick.grid.js
+++ slick.grid.js
@@ -260,11 +260,11 @@ if (typeof Slick === "undefined") {
         createColumnHeaders();
         setupColumnSort();
         createCssRules();
-        resizeAndRender();
+        resizeCanvas();
         bindAncestorScrollEvents();
 
         $container
-            .on("resize.slickgrid", resizeAndRender);
+            .on("resize.slickgrid", resizeCanvas);
         $viewport
             .on("scroll.slickgrid", handleScroll);
         $headerScroller
@@ -975,7 +975,7 @@ if (typeof Slick === "undefined") {
         createColumnHeaders();
         removeCssRules();
         createCssRules();
-        resizeAndRender();
+        resizeCanvas();
         applyColumnWidths();
         handleScroll();
       }
@@ -1276,17 +1276,12 @@ if (typeof Slick === "undefined") {
       viewportW = parseFloat($.css($container[0], "width", true));
       $viewport.height(viewportH);
 
-      updateRowCount();
-      render();
-    }
-
-    function resizeAndRender() {
       if (options.forceFitColumns) {
         autosizeColumns();
-        render();
-      } else {
-        resizeCanvas();
       }
+
+      updateRowCount();
+      render();
     }
 
     function updateRowCount() {
