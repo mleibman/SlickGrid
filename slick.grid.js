0a6cfdb2b85ed5f52159f3fa1c3d50162cf30531
David Capello <davidcapello@gmail.com>
2012-02-22 15:19:58 -0300

    Add getSortColumns() method to SlickGrid.

--- slick.grid.js
+++ slick.grid.js
@@ -1009,6 +1009,10 @@ if (typeof Slick === "undefined") {
       });
     }
 
+    function getSortColumns() {
+      return sortColumns;
+    }
+
     function handleSelectedRangesChanged(e, ranges) {
       selectedRows = [];
       var hash = {};
@@ -2718,6 +2722,7 @@ if (typeof Slick === "undefined") {
       "updateColumnHeader": updateColumnHeader,
       "setSortColumn": setSortColumn,
       "setSortColumns": setSortColumns,
+      "getSortColumns": getSortColumns,
       "autosizeColumns": autosizeColumns,
       "getOptions": getOptions,
       "setOptions": setOptions,
