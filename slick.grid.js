7fa3f3623a1b42fa08ab594bd357b101d4d2ae13
Michael Leibman <michael.leibman@gmail.com>
2012-06-01 10:19:36 -0700

    Added "scrollRowToTop()" and fixed bounds checking in "scrollRowIntoView()".

--- slick.grid.js
+++ slick.grid.js
@@ -1149,6 +1149,9 @@ if (typeof Slick === "undefined") {
     // Rendering / Scrolling
 
     function scrollTo(y) {
+      y = Math.max(y, 0);
+      y = Math.min(y, th - viewportH);
+
       var oldOffset = offset;
 
       page = Math.min(n - 1, Math.floor(y / ph));
@@ -2273,6 +2276,11 @@ if (typeof Slick === "undefined") {
       }
     }
 
+    function scrollRowToTop(row) {
+      scrollTo(row * options.rowHeight);
+      render();
+    }
+
     function getColspan(row, cell) {
       var metadata = data.getItemMetadata && data.getItemMetadata(row);
       if (!metadata || !metadata.columns) {
@@ -2793,6 +2801,7 @@ if (typeof Slick === "undefined") {
       "resizeCanvas": resizeCanvas,
       "updateRowCount": updateRowCount,
       "scrollRowIntoView": scrollRowIntoView,
+      "scrollRowToTop": scrollRowToTop,
       "getCanvasNode": getCanvasNode,
       "focus": setFocus,
 
