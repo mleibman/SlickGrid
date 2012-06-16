cc6a542cf2faa79495c4505a9423d044f473520a
Michael Leibman <michael.leibman@gmail.com>
2012-06-15 18:12:48 -0700

    Fix #377 - vertical bounds checking wasn't accounting for the scrollbar.

--- slick.grid.js
+++ slick.grid.js
@@ -1150,7 +1150,7 @@ if (typeof Slick === "undefined") {
 
     function scrollTo(y) {
       y = Math.max(y, 0);
-      y = Math.min(y, th - viewportH);
+      y = Math.min(y, th - viewportH + (viewportHasHScroll ? scrollbarDimensions.height : 0));
 
       var oldOffset = offset;
 
