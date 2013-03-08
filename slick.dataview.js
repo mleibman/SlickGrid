5601f55deb80b921620f0e34f5bc20b7ba25ab36
Michael Leibman <michael.leibman@gmail.com>
2013-03-07 16:45:26 -0800

    Optimize DataView flattenGroupedRows().

--- slick.dataview.js
+++ slick.dataview.js
@@ -564,20 +564,22 @@
       }
     }
 
-    function flattenGroupedRows(groups) {
+    function flattenGroupedRows(groups, level) {
+      level = level || 0;
+      var gi = groupingInfos[level];
       var groupedRows = [], rows, gl = 0, g;
       for (var i = 0, l = groups.length; i < l; i++) {
         g = groups[i];
         groupedRows[gl++] = g;
 
         if (!g.collapsed) {
-          rows = g.groups ? flattenGroupedRows(g.groups) : g.rows;
+          rows = g.groups ? flattenGroupedRows(g.groups, level + 1) : g.rows;
           for (var j = 0, jj = rows.length; j < jj; j++) {
             groupedRows[gl++] = rows[j];
           }
         }
 
-        if (g.totals && (!g.collapsed || groupingInfos[g.level].aggregateCollapsed)) {
+        if (g.totals && (!g.collapsed || gi.aggregateCollapsed)) {
           groupedRows[gl++] = g.totals;
         }
       }
