b86025073ac08ec05325a537e61abbd8e0c76b9b
Roman Shtylman <shtylman@gmail.com>
2013-03-27 11:13:26 -0400

    remove two step css rule creation
    This has stopped working in google chrome. Moreover, I do not see why it
    was done like this in the first place as the rules can be gotten right
    from the style object after it is populated with text.
    
    TODO long term this is too fragile and should be done a better way.

--- grid.js
+++ grid.js
@@ -946,38 +946,30 @@ var Slick = require('./core');
       } else {
         $style[0].appendChild(document.createTextNode(rules.join(" ")));
       }
+
+      stylesheet = $style[0].sheet;
+
+      // cache column CSS rules
+      columnCssRulesL = [];
+      columnCssRulesR = [];
+      var cssRules = (stylesheet.cssRules || stylesheet.rules);
+      var matches, columnIdx;
+      for (var i = 0; i < cssRules.length; i++) {
+        var selector = cssRules[i].selectorText;
+        if (matches = /\.l\d+/.exec(selector)) {
+          columnIdx = parseInt(matches[0].substr(2, matches[0].length - 2), 10);
+          columnCssRulesL[columnIdx] = cssRules[i];
+        } else if (matches = /\.r\d+/.exec(selector)) {
+          columnIdx = parseInt(matches[0].substr(2, matches[0].length - 2), 10);
+          columnCssRulesR[columnIdx] = cssRules[i];
+        }
+      }
     }
 
     function getColumnCssRules(idx) {
       if (!stylesheet) {
-        var sheets = document.styleSheets;
-        for (var i = 0; i < sheets.length; i++) {
-          if ((sheets[i].ownerNode || sheets[i].owningElement) == $style[0]) {
-            stylesheet = sheets[i];
-            break;
-          }
-        }
-
-        if (!stylesheet) {
-          throw new Error("Cannot find stylesheet.");
-        }
-
-        // find and cache column CSS rules
-        columnCssRulesL = [];
-        columnCssRulesR = [];
-        var cssRules = (stylesheet.cssRules || stylesheet.rules);
-        var matches, columnIdx;
-        for (var i = 0; i < cssRules.length; i++) {
-          var selector = cssRules[i].selectorText;
-          if (matches = /\.l\d+/.exec(selector)) {
-            columnIdx = parseInt(matches[0].substr(2, matches[0].length - 2), 10);
-            columnCssRulesL[columnIdx] = cssRules[i];
-          } else if (matches = /\.r\d+/.exec(selector)) {
-            columnIdx = parseInt(matches[0].substr(2, matches[0].length - 2), 10);
-            columnCssRulesR[columnIdx] = cssRules[i];
-          }
-        }
-      }
+        createCssRules()
+      };
 
       return {
         "left": columnCssRulesL[idx],
