ca3ec08daea2d311680698fdcb0edb67b1c755af
kzhdev <kzhdev@gmail.com>
2013-06-14 22:03:08 -0500

    back out for loop changes

--- slick.grid.js
+++ slick.grid.js
@@ -196,7 +196,7 @@ if (typeof Slick === "undefined") {
       columnDefaults.width = options.defaultColumnWidth;
 
       columnsById = {};
-      for (var i = 0, len = columns.length; i < len; i++) {
+      for (var i = 0; i < columns.length; i++) {
         var m = columns[i] = $.extend({}, columnDefaults, columns[i]);
         columnsById[m.id] = i;
         if (m.minWidth && m.width < m.minWidth) {
