151793bb22d47d826d0db8b68f8bc332aad0cf9d
Rand Scullard <rand@randscullard.com>
2013-06-25 10:52:14 -0400

    Fix issue #653 by prepending the sort indicator rather than appending it.

--- slick.grid.js
+++ slick.grid.js
@@ -564,7 +564,7 @@ if (typeof Slick === "undefined") {
 
         if (m.sortable) {
           header.addClass("slick-header-sortable");
-          header.append("<span class='slick-sort-indicator' />");
+          header.prepend("<span class='slick-sort-indicator' />");
         }
 
         trigger(self.onHeaderCellRendered, {
