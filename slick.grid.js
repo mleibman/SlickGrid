f66327e2ba8037a93c3edc28f7872b14ac333da2
Takahiro3Kubo <Takahiro3.Kubo@primepolymer.co.jp>
2013-06-28 14:47:43 +0900

    fix issue #563

--- slick.grid.js
+++ slick.grid.js
@@ -2456,7 +2456,10 @@ if (typeof Slick === "undefined") {
 
     function clearTextSelection() {
       if (document.selection && document.selection.empty) {
-        document.selection.empty();
+		try {
+				//IE fails here if selected element is not in dom
+				document.selection.empty();
+            } catch (e) { }
       } else if (window.getSelection) {
         var sel = window.getSelection();
         if (sel && sel.removeAllRanges) {
