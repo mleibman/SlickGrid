64bb5866b00807fa6537b0b176b4a92abf26c14a
Graeme Pyle <graeme@graemep.me>
2013-12-11 22:03:19 +0200

    Handle exceptions from formatters

--- slick.grid.js
+++ slick.grid.js
@@ -1450,7 +1450,18 @@ if (typeof Slick === "undefined") {
       // if there is a corresponding row (if not, this is the Add New row or this data hasn't been loaded yet)
       if (item) {
         var value = getDataItemValueForColumn(item, m);
-        stringArray.push(getFormatter(row, m)(row, cell, value, m, item));
+
+        try {
+
+          var html=getFormatter(row, m)(row, cell, value, m, item);
+          stringArray.push(html);
+
+        } catch (e) {
+
+          console.error("Exception thrown in formatter ["+e+"], ignoring. Stack trace: ", e.stack);
+
+        }
+
       }
 
       stringArray.push("</div>");
