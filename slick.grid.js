bf21472f2aa0c1f1b2278db591906c5b349fe557
Anil Rao <anil.rao@tradingtechnologies.com>
2014-04-02 16:34:23 -0500

    DEB-3858 Fixed "Cannot find stylesheet" issue - Fix obtained from GitHub SlickGrid Issue #223 - Refer to https://github.com/mleibman/SlickGrid/issues/223 for details

--- slick.grid.js
+++ slick.grid.js
@@ -920,24 +920,32 @@ if (typeof Slick === "undefined") {
     function createCssRules() {
       $style = $("<style type='text/css' rel='stylesheet' />").appendTo($("head"));
       var rowHeight = (options.rowHeight - cellHeightDiff);
-      var rules = [
-        "." + uid + " .slick-header-column { left: 1000px; }",
-        "." + uid + " .slick-top-panel { height:" + options.topPanelHeight + "px; }",
-        "." + uid + " .slick-headerrow-columns { height:" + options.headerRowHeight + "px; }",
-        "." + uid + " .slick-cell { height:" + rowHeight + "px; }",
-        "." + uid + " .slick-row { height:" + options.rowHeight + "px; }"
-      ];
-
-      for (var i = 0; i < columns.length; i++) {
-        rules.push("." + uid + " .l" + i + " { }");
-        rules.push("." + uid + " .r" + i + " { }");
-      }
-
       if ($style[0].styleSheet) { // IE
-        $style[0].styleSheet.cssText = rules.join(" ");
+        $style[0].styleSheet.cssText = "";
       } else {
-        $style[0].appendChild(document.createTextNode(rules.join(" ")));
+        $style[0].appendChild(document.createTextNode(""));
       }
+        var sheet =  $style[0].sheet;
+        var index = 0;
+        addCSSRule(sheet,"." + uid + " .slick-header-column", "left: 1000px;",index++);
+        addCSSRule(sheet,"." + uid + " .slick-top-panel", "height:" + options.topPanelHeight + "px;",index++);
+        addCSSRule(sheet,"." + uid + " .slick-headerrow-columns", "height:" + options.headerRowHeight + "px;",index++);
+        addCSSRule(sheet,"." + uid + " .slick-cell", "height:" + rowHeight + "px;",index++);
+        addCSSRule(sheet,"." + uid + " .slick-row", "height:" + options.rowHeight + "px;",index++);
+
+        for (var i = 0; i < columns.length; i++) {
+            addCSSRule(sheet,"." + uid + " .l" + i , "",index++);
+            addCSSRule(sheet,"." + uid + " .r" + i, "",index++);
+          }
+    }
+
+    function addCSSRule(sheet, selector, rules, index) {
+        if(sheet.insertRule) {
+            sheet.insertRule(selector + "{" + rules + "}", index);
+        }
+        else {
+            sheet.addRule(selector, rules, index);
+        }
     }
 
     function getColumnCssRules(idx) {
