2643edcbb1bd9a7d74d57efa1dfe0b8a1c2574c0
Ben McIntyre <email.ben.mcintyre@gmail.com>
2015-06-03 01:18:32 +0930

    Patch absBox for null element bug (MLeibman #1066), comment out workaround for IE 8 accordion bug by default

--- slick.grid.js
+++ slick.grid.js
@@ -2706,6 +2706,8 @@ if (typeof Slick === "undefined") {
       // walk up the tree
       var offsetParent = elem.offsetParent;
       while ((elem = elem.parentNode) != document.body) {
+		if (elem == null) break;
+		
         if (box.visible && elem.scrollHeight != elem.offsetHeight && $(elem).css("overflowY") != "visible") {
           box.visible = box.bottom > elem.scrollTop && box.top < elem.scrollTop + elem.clientHeight;
         }
@@ -3341,7 +3343,7 @@ if (typeof Slick === "undefined") {
     // Public API
 
     $.extend(this, {
-      "slickGridVersion": "2.2.3",
+      "slickGridVersion": "2.2.4",
 
       // Events
       "onScroll": new Slick.Event(),
