b7ca1510062b7754925704bb325e877d9a3b0566
Ben McIntyre <email.ben.mcintyre@gmail.com>
2015-08-14 11:22:18 +0930

    allow null column setting to override editor default

--- slick.editors.js
+++ slick.editors.js
@@ -181,7 +181,7 @@
 	function getDecimalPlaces() {
 		// returns the number of fixed decimal places or null
 		var rtn = args.column.editorFixedDecimalPlaces;
-		if (!rtn && rtn!==0) { 
+		if (typeof rtn == 'undefined') { 
 			rtn = FloatEditor.DefaultDecimalPlaces;
 		}
 		return (!rtn && rtn!==0 ? null : rtn);
