mleibman/SlickGrid

commit c30867d02b269674ab27f2fffa755c7339fc8ff0

make pager text an option

controls/slick.pager.js
@@ -1,8 +1,15 @@
 (function ($) {
-  function SlickGridPager(dataView, grid, $container) {
+  function SlickGridPager(dataView, grid, $container, options) {
     var $status;
-
+    var _options;
+    var _defaults = {
+      showAllText: "Showing all {rowCount} rows",
+      showPageText: "Showing page {pageNum} of {pageCount}"
+    };
+    
     function init() {
+      _options = $.extend(true, {}, _defaults, options);
+      
       dataView.onPagingInfoChanged.subscribe(function (e, pagingInfo) {
         updatePager(pagingInfo);
       });
@@ -133,9 +140,9 @@
       }
 
       if (pagingInfo.pageSize == 0) {
-        $status.text("Showing all " + pagingInfo.totalRows + " rows");
+        $status.text(_options.showAllText.replace('{rowCount}', pagingInfo.totalRows + "").replace('{pageCount}', pagingInfo.totalPages + ""));
       } else {
-        $status.text("Showing page " + (pagingInfo.pageNum + 1) + " of " + pagingInfo.totalPages);
+        $status.text(_options.showPageText.replace('{pageNum}', pagingInfo.pageNum + "").replace('{pageCount}', pagingInfo.totalPages + ""));
       }
     }
 

commit 8d6c6c7fad2cd43a7d496062ed5b37916bfbb99a

Fix displayed page number being zero-based. The displayed page number was zero based, meaning that the pager would show something like "Showing page 0 of 1".
This is fixed by adding one. I believe this was a bug introduced in c30867d02b269674ab27f2fffa755c7339fc8ff0 (before which, one was added to the page number to be displayed).

controls/slick.pager.js
@@ -142,7 +142,7 @@
       if (pagingInfo.pageSize == 0) {
         $status.text(_options.showAllText.replace('{rowCount}', pagingInfo.totalRows + "").replace('{pageCount}', pagingInfo.totalPages + ""));
       } else {
-        $status.text(_options.showPageText.replace('{pageNum}', pagingInfo.pageNum + "").replace('{pageCount}', pagingInfo.totalPages + ""));
+        $status.text(_options.showPageText.replace('{pageNum}', pagingInfo.pageNum + 1 + "").replace('{pageCount}', pagingInfo.totalPages + ""));
       }
     }
 