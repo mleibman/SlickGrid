a770835e50a5a6908123e2d2457910f22094f6f5
Ben McIntyre <email.ben.mcintyre@gmail.com>
2018-02-24 09:15:15 +0930

    Add emulatePagingWhenScrolling option so when scrolling down with downarrow, we have a choice about whether, when we reach the bottom row in the viewpost, to scroll a whole page down and relocate the active row to the top line (true) or just to move the viewport contents up one row (false)

--- slick.grid.js
+++ slick.grid.js
@@ -100,7 +100,8 @@ if (typeof Slick === "undefined") {
       preserveCopiedSelectionOnPaste: false,
       showCellSelection: true,
       viewportClass: null,
-	  minRowBuffer: 3
+      minRowBuffer: 3,
+      emulatePagingWhenScrolling: true
     };
 
     var columnDefaults = {
@@ -3598,7 +3599,7 @@ if (typeof Slick === "undefined") {
       var pos = stepFn(activeRow, activeCell, activePosX);
       if (pos) {
         var isAddNewRow = (pos.row == getDataLength());
-        scrollCellIntoView(pos.row, pos.cell, !isAddNewRow);
+        scrollCellIntoView(pos.row, pos.cell, !isAddNewRow && options.emulatePagingWhenScrolling);
         setActiveCellInternal(getCellNode(pos.row, pos.cell));
         activePosX = pos.posX;
         return true;
