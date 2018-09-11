f806ad0f14d8c754108d14f80c60bbd3e4d5b6ca
Ghislain B <gbeaulac@gmail.com>
2018-09-11 00:23:59 -0400

    feat(checkbox): add way to show Checbox Selector in Filter Row - add a way to show the Checkbox Selector (select all) inside the Header Row (aka Filter row) via a new flag boolean "showInFilterHeaderRow" false by defaults - keep previous behavior of showing Checkbox Select in the Column Header Title Row, via a new flag "showInColumnTitleRow", true by default - both flags can be used (and tested) together, but most often used separately

--- plugins/slick.checkboxselectcolumn.js
+++ plugins/slick.checkboxselectcolumn.js
@@ -9,7 +9,7 @@
 
   function CheckboxSelectColumn(options) {
     var _grid;
-    var _self = this;
+    var _selectAll_UID = createUID();
     var _handler = new Slick.EventHandler();
     var _selectedRowsLookup = {};
     var _defaults = {
@@ -17,7 +17,9 @@
       cssClass: null,
       hideSelectAllCheckbox: false,
       toolTip: "Select/Deselect All",
-      width: 30
+      width: 30,
+      showInColumnTitleRow: true,
+      showInFilterHeaderRow: false
     };
     var _isSelectAllChecked = false;
 
@@ -26,36 +28,48 @@
     function init(grid) {
       _grid = grid;
       _handler
-        .subscribe(_grid.onSelectedRowsChanged, handleSelectedRowsChanged)
-        .subscribe(_grid.onClick, handleClick)
-        .subscribe(_grid.onHeaderClick, handleHeaderClick)
-        .subscribe(_grid.onKeyDown, handleKeyDown);
+      .subscribe(_grid.onSelectedRowsChanged, handleSelectedRowsChanged)
+      .subscribe(_grid.onClick, handleClick)
+      .subscribe(_grid.onKeyDown, handleKeyDown);
+      
+      if (_options.showInFilterHeaderRow) {
+        addCheckboxToFilterHeaderRow(grid);
+      }
+      if (_options.showInColumnTitleRow) {
+        _handler.subscribe(_grid.onHeaderClick, handleHeaderClick)
+      }
     }
 
     function destroy() {
       _handler.unsubscribeAll();
     }
 
-    function getOptions(options) {
+    function getOptions() {
       return _options;
     }
 
     function setOptions(options) {
       _options = $.extend(true, {}, _options, options);
+      
       if (_options.hideSelectAllCheckbox) {
         _grid.updateColumnHeader(_options.columnId, "", "");
+        $("#header-filter-selector" + _selectAll_UID).hide();
       } else {
-        var UID = createUID();
-        if (_isSelectAllChecked) {
-          _grid.updateColumnHeader(_options.columnId, "<input id='header-selector" + UID + "' type='checkbox' checked='checked'><label for='header-selector" + UID + "'></label>", _options.toolTip);
-        } else {
-          _grid.updateColumnHeader(_options.columnId, "<input id='header-selector" + UID + "' type='checkbox'><label for='header-selector" + UID + "'></label>", _options.toolTip);
+        if (_options.showInColumnTitleRow) {
+          if (_isSelectAllChecked) {
+            _grid.updateColumnHeader(_options.columnId, "<input id='header-selector" + _selectAll_UID + "' type='checkbox' checked='checked'><label for='header-selector" + _selectAll_UID + "'></label>", _options.toolTip);
+          } else {
+            _grid.updateColumnHeader(_options.columnId, "<input id='header-selector" + _selectAll_UID + "' type='checkbox'><label for='header-selector" + _selectAll_UID + "'></label>", _options.toolTip);
+          }
         }
-      }
+        if (_options.showInFilterHeaderRow) {
+          var selectAllElm = $("#header-filter-selector" + _selectAll_UID).show();
+          selectAllElm.prop("checked", _isSelectAllChecked);
+        }
+      } 
     }
 
     function handleSelectedRowsChanged(e, args) {
-      var UID = createUID();
       var selectedRows = _grid.getSelectedRows();
       var lookup = {}, row, i;
       for (i = 0; i < selectedRows.length; i++) {
@@ -73,12 +87,16 @@
       _grid.render();
       _isSelectAllChecked = selectedRows.length && selectedRows.length == _grid.getDataLength();
 
-      if (!_options.hideSelectAllCheckbox) {
+      if (_options.showInColumnTitleRow && !_options.hideSelectAllCheckbox) {
         if (_isSelectAllChecked) {
-          _grid.updateColumnHeader(_options.columnId, "<input id='header-selector" + UID + "' type='checkbox' checked='checked'><label for='header-selector" + UID + "'></label>", _options.toolTip);
+          _grid.updateColumnHeader(_options.columnId, "<input id='header-selector" + _selectAll_UID + "' type='checkbox' checked='checked'><label for='header-selector" + _selectAll_UID + "'></label>", _options.toolTip);
         } else {
-          _grid.updateColumnHeader(_options.columnId, "<input id='header-selector" + UID + "' type='checkbox'><label for='header-selector" + UID + "'></label>", _options.toolTip);
+          _grid.updateColumnHeader(_options.columnId, "<input id='header-selector" + _selectAll_UID + "' type='checkbox'><label for='header-selector" + _selectAll_UID + "'></label>", _options.toolTip);
         }
+      } 
+      if (_options.showInFilterHeaderRow) {
+        var selectAllElm = $("#header-filter-selector" + _selectAll_UID);
+        selectAllElm.prop("checked", _isSelectAllChecked);
       }
     }
 
@@ -184,11 +202,9 @@
     }
 
     function getColumnDefinition() {
-      var UID = createUID();
-
       return {
         id: _options.columnId,
-        name: _options.hideSelectAllCheckbox ? "" : "<input id='header-selector" + UID + "' type='checkbox'><label for='header-selector" + UID + "'></label>",
+        name: (_options.hideSelectAllCheckbox || !_options.showInColumnTitleRow) ? "" : "<input id='header-selector" + _selectAll_UID + "' type='checkbox'><label for='header-selector" + _selectAll_UID + "'></label>",
         toolTip: _options.toolTip,
         field: "sel",
         width: _options.width,
@@ -198,6 +214,19 @@
         hideSelectAllCheckbox: _options.hideSelectAllCheckbox,
         formatter: checkboxSelectionFormatter
       };
+    }
+
+    function addCheckboxToFilterHeaderRow(grid) {
+      grid.onHeaderRowCellRendered.subscribe(function(e, args) {
+        if (args.column.field === "sel") {
+          $(args.node).empty();
+          $("<input id='header-filter-selector" + _selectAll_UID + "' type='checkbox'><label for='header-selector" + _selectAll_UID + "'></label>")
+            .appendTo(args.node)
+            .on('click', function(evnt) { 
+              handleHeaderClick(evnt, args) 
+            });
+        }
+      });
     }
 
     function createUID() {
