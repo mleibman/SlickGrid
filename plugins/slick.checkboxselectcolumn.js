8b5c3f254718e5381c3b35f3713dce3fa8f0b752
Ghislain B <gbeaulac@gmail.com>
2018-09-11 13:01:24 -0400

    fix handle click from title not always working when using setOptions

--- plugins/slick.checkboxselectcolumn.js
+++ plugins/slick.checkboxselectcolumn.js
@@ -61,6 +61,7 @@
           } else {
             _grid.updateColumnHeader(_options.columnId, "<input id='header-selector" + _selectAll_UID + "' type='checkbox'><label for='header-selector" + _selectAll_UID + "'></label>", _options.toolTip);
           }
+          _handler.subscribe(_grid.onHeaderClick, handleHeaderClick);
         } else {
           hideSelectAllFromColumnHeaderTitleRow();
         }
