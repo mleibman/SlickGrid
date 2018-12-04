d9ce5d843fd674fa9ebf7d28cafaf0366db5aac9
Ben McIntyre <email.ben.mcintyre@gmail.com>
2018-12-04 21:02:30 +0930

    update keycode constant to remove jQuery dependency

--- slick.groupitemmetadataprovider.js
+++ slick.groupitemmetadataprovider.js
@@ -112,7 +112,7 @@
 
     // TODO:  add -/+ handling
     function handleGridKeyDown(e, args) {
-      if (options.enableExpandCollapse && (e.which == $.ui.keyCode.SPACE)) {
+      if (options.enableExpandCollapse && (e.which == Slick.keyCode.SPACE)) {
         var activeCell = this.getActiveCell();
         if (activeCell) {
           var item = this.getDataItem(activeCell.row);
