43988e6423bfb66cd589fa30539ebabd3da8cf5d
ghiscoding <gbeaulac@gmail.com>
2018-12-08 00:35:00 -0500

    remove redundant notify since it should only be called in updateGroupBy - because the notify is now in updateGroupBy(), the notify in clearDroppedGroups() becomes redundant is unnecessary

--- plugins/slick.draggablegrouping.js
+++ plugins/slick.draggablegrouping.js
@@ -270,7 +270,6 @@
     function clearDroppedGroups() {
       columnsGroupBy = [];
       updateGroupBy();
-      onGroupChanged.notify({ groupColumns: []})
       dropbox.find(".slick-dropped-grouping").remove();
       groupToggler.css("display", "none");
       dropboxPlaceholder.show()
