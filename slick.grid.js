7b03a06c2be3266cec1503fef38168b0d533f39d
Ben McIntyre <email.ben.mcintyre@gmail.com>
2018-12-21 07:10:10 +0930

    Change option name to alwaysAllowHorizontalScroll as per #307

--- slick.grid.js
+++ slick.grid.js
@@ -56,7 +56,7 @@ if (typeof Slick === "undefined") {
     // settings
     var defaults = {
       alwaysShowVerticalScroll: false,
-      alwaysShowHorizontalScroll: false,
+      alwaysAllowHorizontalScroll: false,
       explicitInitialization: false,
       rowHeight: 25,
       defaultColumnWidth: 80,
@@ -309,7 +309,7 @@ if (typeof Slick === "undefined") {
 
       $viewport = $("<div class='slick-viewport' style='width:100%;overflow:auto;outline:0;position:relative;;'>").appendTo($container);
       $viewport.css("overflow-y", options.alwaysShowVerticalScroll ? "scroll" : (options.autoHeight ? "hidden" : "auto"));
-      $viewport.css("overflow-x", options.forceFitColumns && !options.alwaysShowHorizontalScroll ? "hidden" : "auto");
+      $viewport.css("overflow-x", options.forceFitColumns && !options.alwaysAllowHorizontalScroll ? "hidden" : "auto");
       if (options.viewportClass) $viewport.toggleClass(options.viewportClass, true);
 
       $canvas = $("<div class='grid-canvas' />").appendTo($viewport);
