66f9a207ab267cf53d620b1e53e330d7c09625f2
Ben McIntyre <email.ben.mcintyre@gmail.com>
2019-09-17 08:40:01 +0930

    release v2.4.14

--- slick.grid.js
+++ slick.grid.js
@@ -5713,7 +5713,7 @@ if (typeof Slick === "undefined") {
     // Public API
 
     $.extend(this, {
-      "slickGridVersion": "2.4.13",
+      "slickGridVersion": "2.4.14",
 
       // Events
       "onScroll": new Slick.Event(),
