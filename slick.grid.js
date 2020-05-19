4d6b92997495fc160ab696d0cb5a0447a0290d93
Ben McIntyre <email.ben.mcintyre@gmail.com>
2020-05-19 16:09:01 +0930

    release v2.4.23

--- slick.grid.js
+++ slick.grid.js
@@ -5765,7 +5765,7 @@ if (typeof Slick === "undefined") {
     // Public API
 
     $.extend(this, {
-      "slickGridVersion": "2.4.22",
+      "slickGridVersion": "2.4.23",
 
       // Events
       "onScroll": new Slick.Event(),
